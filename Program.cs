3e92505f3d3c82b6d2fd5ff87c22de125fda1918
Piotr Karpala <piotrkarpala@microsoft.com>
2024-03-18 00:53:51 -0400

    init commit
    Signed-off-by: Piotr Karpala <piotrkarpala@microsoft.com>

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+class Program
+{
+    const string connectionString = "<your_connection_string>";
+    const string queueName = "<your_queue_name>";
+
+    static async Task Main(string[] args)
+    {
+        await using var client = new ServiceBusClient(connectionString);
+
+        // create a processor that we can use to process the messages
+        ServiceBusProcessor processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+        // add handler to process messages
+        processor.ProcessMessageAsync += MessageHandler;
+
+        // add handler to process any errors
+        processor.ProcessErrorAsync += ErrorHandler;
+
+        // start processing 
+        await processor.StartProcessingAsync();
+
+        Console.WriteLine("Press any key to stop the processing");
+        Console.ReadKey();
+
+        // stop processing 
+        await processor.StopProcessingAsync();
+    }
+
+    static Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        string body = args.Message.Body.ToString();
+        Console.WriteLine($"Received: {body}");
+
+        return Task.CompletedTask;
+    }
+
+    static Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.WriteLine(args.Exception.ToString());
+        return Task.CompletedTask;
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+
+Service Bus defferals.
+
+## Context
+
+> We have a long process to encode data from our record files into 2 different file formats. 1 can take up to an hour or more. Due to the nature of Service bus not being able to guarantee a lock renewal, we opted to defer the message while processing occurs, then complete the message after the encode process finishes. When the message stays in context and we defer it, I assumed the lock becomes irrelevant since no other message would be read by the processor unless they have the sequence number context? Our current implementation uses a ServiceBusProcessor client and ServiceBusReceiver client to achieve this, but I was wondering if we could just remove the Receiver if the message stays in context.
